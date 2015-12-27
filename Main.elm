052adf8ca3679602e346c610711a2771427fec75
joschout <jonas.schouterden@student.kuleuven.be>
2015-12-27 17:50:00 +0100

    Updated the comments in Main.elm to reflect the changes

--- Main.elm
+++ Main.elm
@@ -18,6 +18,8 @@ import Initial exposing (initialItemList)
 -- 1) there are items marked as done
 -- 2) the visibility of the done section is on
 --    (Hotkey: ALT + I)
+-- Note: which item will be focused on using the hotkeys to focus on the next and previous item
+-- depends on the visibility of the done section.
 
 
 -- * Hide the 'add reminder' functionality and add a hotkey to toggle its
@@ -48,8 +50,16 @@ import Initial exposing (initialItemList)
 -- * Add a 'snooze' feature to items, to 'snooze' an item you must provide a
 -- * date on which the item has to 'un-snooze'. 'snoozed' items are not visible.
 -- Status: Completed / Attempted / Unattempted
--- Summary:
-
+-- Summary: Completed
+-- If a date earlier than the current date is picked, the item stays visible.
+-- If a date later than the current date is picked, the item disappears
+-- the to do or done section.
+-- EXTRA: The snoozed items can be shown using the ALT + W hotkey
+-- This will make a new section visible below the Done section.
+-- In this section, the snoozed items are listed, together with the date until
+-- which each item is snoozed.
+-- Note: which item will be focused on using the hotkeys to focus on the next and previous item
+-- depends on the visibility of the Snoozed section.
 
 -- * On startup, read e-mails from a Json document at this url:
 -- * http://people.cs.kuleuven.be/~bob.reynders/2015-2016/emails.json
@@ -70,7 +80,9 @@ import Initial exposing (initialItemList)
 
 -- * Come up with your own extension!
 -- Status: Completed / Attempted / Unattempted
--- Summary:
+-- Summary: Completed
+-- A section to see all the snoozed items can be toggled using ALT + W
+-- See also the summary of the snoozed item feature.
 
 
 -- Start of program
