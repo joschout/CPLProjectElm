d70e54b21a4fefbae0eddfedc3ad039d7454d9e8
joschout <jonas.schouterden@student.kuleuven.be>
2015-12-29 16:32:36 +0100

    basic code to read JSOn from the URL in the module JSONUtil.elm

--- JSONUtil.elm
+++ JSONUtil.elm
@@ -1,35 +1,60 @@
+module JSONUtil
+  (jsonMailbox)
+  where
+
 import Http
-import Markdown
 import Html exposing (Html)
 import Task exposing (Task, andThen)
-
-
-main : Signal Html
-main =
-  Signal.map Markdown.toHtml readme.signal
-
+import Json.Decode exposing ((:=))
 
 -- set up mailbox
 --   the signal is piped directly to main
 --   the address lets us update the signal
-readme : Signal.Mailbox String
-readme =
-  Signal.mailbox ""
+jsonMailbox : Signal.Mailbox (List (EmailKVString))
+jsonMailbox =
+  Signal.mailbox []
 
+-- the URL where our JSON lives
+urlOfJSON : String
+urlOfJSON = "https://api.myjson.com/bins/19lg3"
 
--- send some markdown to our readme mailbox
-report : String -> Task x ()
-report markdown =
-  Signal.send readme.address markdown
+-- type alias to make my life easier
+type alias EmailKVString = List (String, String)
 
-
--- get the readme *and then* send the result to our mailbox
+-- get the email info *and then* send the result to our mailbox
 port fetchReadme : Task Http.Error ()
 port fetchReadme =
-  Http.getString readmeUrl `andThen` report
+  safeGetJSOn `andThen` sendJSON
 
+-- TASK FOR SENDING THE EMAIL INFO TO THE MAILBOX ------------------------------
+-- send the list of email info to our jsonMailbox
+sendJSON : List (EmailKVString) -> Task x ()
+sendJSON emailKVList =
+  Signal.send jsonMailbox.address emailKVList
 
--- the URL of the README.md that we desire
-readmeUrl : String
-readmeUrl =
-  "https://raw.githubusercontent.com/elm-lang/core/master/README.md"
+-- TASKS FOR GETTING THE EMAIL INFORMATION -------------------------------------
+-- get the list of EmailKVString values at the url
+-- Note: this function may fail
+getJSON : Task Http.Error (List EmailKVString)
+getJSON =
+  Http.get emailsJSONDecoder urlOfJSON
+
+-- get the list of EmailKVString values at the url
+-- If it should failm return an empty lis.
+safeGetJSOn : Task x (List EmailKVString)
+safeGetJSOn =
+  getJSON `Task.onError` (\err -> Task.succeed [])
+
+-- DECODING THE JSON -----------------------------------------------------------
+-- decoder for the String key-value pairs which make up the information of an email
+emailFieldsJSONDecoder : Json.Decode.Decoder EmailKVString
+emailFieldsJSONDecoder = Json.Decode.keyValuePairs Json.Decode.string
+
+-- decoder for an email list
+emailListJSONDecoder : Json.Decode.Decoder (List EmailKVString)
+emailListJSONDecoder
+  = Json.Decode.list emailFieldsJSONDecoder
+
+-- decoder forthe list of emails as found at the URL
+emailsJSONDecoder : Json.Decode.Decoder (List EmailKVString)
+emailsJSONDecoder = "emails" := emailListJSONDecoder
